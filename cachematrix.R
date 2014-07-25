74a8497ac9d5ae5de317ead14a09421896bf4362
adam <haras.adam@gmail.com>
2014-07-25 20:55:30 +0200

    Add caching and inversing functions
    Add implementation for caching and inversing functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow to cache inversed matrix to avoid duplicating
+## computations
 
-## Write a short comment describing this function
-
+## This function is responsible for storing matrix and inversed matrix
+## as well as for providing access to them
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function checks if in cache is inversed matrix and then
+## served it, if not matrix inversion is performed and result is
+## additionally saved in cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
